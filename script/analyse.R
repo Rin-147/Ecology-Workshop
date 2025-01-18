8f2f101d3323315b215d73b1064d381de1176e72
Crina Malanciuc <195580981+Rin-147@users.noreply.github.com>
2025-01-18 15:42:25 -0500

    added color for sex

--- script/analyse.R
+++ script/analyse.R
@@ -2,5 +2,5 @@
 library(tidyverse)
 library("ratdat")
 
-ggplot(data=complete_old, aes (x=weight, y = hindfoot_length))+
+ggplot(data=complete_old, aes (x=weight, y = hindfoot_length, color=sex))+
   geom_point(color = "green", alpha=0.2)
